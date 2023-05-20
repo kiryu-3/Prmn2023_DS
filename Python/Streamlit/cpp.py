7ee8656ee9677bf780b08670bf5c1dadc46d63ac
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-05-21 08:40:12 +0900

    Update cpp.py

--- Python/Streamlit/cpp.py
+++ Python/Streamlit/cpp.py
@@ -41,9 +41,10 @@ with right:
     st.subheader("地図の全描画データ")
     st.write(data["all_drawings"])
     if "all_drawings" in st_data:
-        if 0 in st_data["all_drawings"][0]:
+        if st_data["all_drawings"][0] is not None and 0 in st_data["all_drawings"][0]:
             st_data["all_drawings"][0][0]["properties"] = "0"
-            st_data["all_drawings"][0]
+             st.subheader("抜粋データ")
+            st.write(st_data["all_drawings"][0])
          
     st.subheader("最後に描画した円の半径データ")
     st.write(data["last_circle_radius"])
