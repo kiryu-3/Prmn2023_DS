5230499313148574eaf92aa98ecf4c1e83fbbad2
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-05-21 08:50:24 +0900

    Update cpp.py

--- Python/Streamlit/cpp.py
+++ Python/Streamlit/cpp.py
@@ -40,7 +40,7 @@ with right:
     st.write(data)
     st.subheader("地図の全描画データ")
     st.write(data["all_drawings"])
-    st.write([item for item in data if item.get("0") is not None])
+    st.write([data[i] for i in data if i == 0])
     if "all_drawings" in data:
         if data["all_drawings"] is not None and 0 in data["all_drawings"][0]:
             data["all_drawings"][0][0]["properties"] = "0"
