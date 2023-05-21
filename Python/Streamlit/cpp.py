fc592526d1aab867c274d753d7a0d9decea726ea
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-05-21 09:01:44 +0900

    Update cpp.py

--- Python/Streamlit/cpp.py
+++ Python/Streamlit/cpp.py
@@ -47,8 +47,20 @@ with right:
             data["all_drawings"][0]["properties"] = "0"
             st.subheader("抜粋データ")
             st.write(data["all_drawings"][0])
+            
+            m = folium.Map(location=[42.793553, 141.6958724], zoom_start=16)
+            folium.Marker(
+                [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+            ).add_to(m)
+            # Leaflet.jsのDrawプラグインを追加
+            draw_options = {'polyline': True, 'rectangle': True, 'circle': True, 'marker': True, 'circlemarker': True}
+            draw = folium.plugins.Draw(export=True, filename='data.geojson', position='topleft', draw_options=draw_options)
+            draw.add_to(m)
+
+            # call to render Folium map in Streamlit
+            st_folium(m, width=725)
             # GeoJSONデータをマップに追加する
-            folium.GeoJson(data).add_to(m)
+            # folium.GeoJson(data).add_to(m)
          
     st.subheader("最後に描画した円の半径データ")
     st.write(data["last_circle_radius"])
