5b63f05186a7ed0d9d9fb577da56105548a83719
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-05-21 09:47:53 +0900

    Update dpp.py

--- Python/Streamlit/dpp.py
+++ Python/Streamlit/dpp.py
@@ -1,13 +1,10 @@
 import io
-import sys
 import pandas as pd
 import folium
 
 import streamlit as st
-import json
-from folium import plugins
+from folium.plugins import TimestampedGeoJson
 from streamlit_folium import folium_static
-from folium.plugins import Draw, TimestampedGeoJson
 
 hide_menu_style = """
     <style>
@@ -18,31 +15,31 @@ hide_menu_style = """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 with st.sidebar:
-  # CSVファイルのアップロード
-  uploaded_csvfile = st.file_uploader("CSVファイルをアップロード", type=["csv"])
-  
-if 'map' not in st.session_state: # 初期化
-  # 初めての表示時は空のマップを表示
-  m = folium.Map(location=[34.797345395117546, 137.5804696201213], zoom_start=13)
+    # CSVファイルのアップロード
+    uploaded_csvfile = st.file_uploader("CSVファイルをアップロード", type=["csv"])
 
-  # Leaflet.jsのDrawプラグインを追加
-  draw_options = {'polyline': True, 'rectangle': True, 'circle': False, 'marker': False, 'circlemarker': False}
-  draw = folium.plugins.Draw(export=True, filename='data.geojson', position='topleft', draw_options=draw_options)
-  draw.add_to(m)
+if 'map' not in st.session_state:  # 初期化
+    # 初めての表示時は空のマップを表示
+    m = folium.Map(location=[34.797345395117546, 137.5804696201213], zoom_start=13)
 
-  # 地図をフルスクリーンに切り替えボタン設置
-  plugins.Fullscreen(
-    position="topright",  # bottomleft 
-    title="拡大する",      
-    title_cancel="元に戻す",
-    force_separate_button=True,
-  ).add_to(m)
+    # Leaflet.jsのDrawプラグインを追加
+    draw_options = {'polyline': True, 'rectangle': True, 'circle': False, 'marker': False, 'circlemarker': False}
+    draw = folium.plugins.Draw(export=True, filename='data.geojson', position='topleft', draw_options=draw_options)
+    draw.add_to(m)
 
-  st.session_state['map'] = m
+    # 地図をフルスクリーンに切り替えボタン設置
+    plugins.Fullscreen(
+        position="topright",  # bottomleft
+        title="拡大する",
+        title_cancel="元に戻す",
+        force_separate_button=True,
+    ).add_to(m)
+
+    st.session_state['map'] = m
 
 if 'geojson_added' not in st.session_state:
     st.session_state['geojson_added'] = False
-    
+
 if uploaded_csvfile is not None:
     file_data = uploaded_csvfile.read()
 
@@ -71,7 +68,7 @@ if uploaded_csvfile is not None:
         features.append(feature)
 
     geojson = {
-        "type": "LineString",
+        "type": "FeatureCollection",
         "features": features
     }
 
@@ -82,9 +79,7 @@ if uploaded_csvfile is not None:
             layers_to_remove.append(layer.get_name())
 
     for layer_name in layers_to_remove:
-        del st.session_state['map']._children[layer_name]
-       
-    
+        st.session_state['map'].get_root().remove_child(st.session_state['map']._children[layer_name])
 
     timestamped_geojson = TimestampedGeoJson(
         geojson,
@@ -96,16 +91,15 @@ if uploaded_csvfile is not None:
 
     # TimestampedGeoJsonをマップに追加
     timestamped_geojson.add_to(st.session_state['map'])
-    
+
 left, right = st.columns(2)
 
-
 with left:
-    with st.echo():    
-        
+    with st.echo():
         # st_data = folium_static(st.session_state['map'])
         st_data = folium_static(st.session_state['map'], width=725)
-    with right:
-        data = dict(st_data)
-        st.subheader("地図の全データ")
-        st.write(data)
+
+with right:
+    data = dict(st_data)
+    st.subheader("地図の全データ")
+    st.write(data)
