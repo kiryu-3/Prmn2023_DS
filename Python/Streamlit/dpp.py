5faadc637cf29787fb2b13ff88211e2dba5196e2
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-05-21 09:56:08 +0900

    Update dpp.py

--- Python/Streamlit/dpp.py
+++ Python/Streamlit/dpp.py
@@ -20,27 +20,25 @@ left, right = st.columns(2)
 
 
 with left:
-    with st.echo():
+    import folium
+    import streamlit as st
 
-        import folium
-        import streamlit as st
+    from streamlit_folium import st_folium
+    from folium import plugins
+    from folium.plugins import Draw, TimestampedGeoJson
 
-        from streamlit_folium import st_folium
-        from folium import plugins
-        from folium.plugins import Draw, TimestampedGeoJson
+    # center on Liberty Bell, add marker
+    m = folium.Map(location=[42.793553, 141.6958724], zoom_start=16)
+    folium.Marker(
+        [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+    ).add_to(m)
+    # Leaflet.jsのDrawプラグインを追加
+    draw_options = {'polyline': True, 'rectangle': True, 'circle': True, 'marker': True, 'circlemarker': True}
+    draw = folium.plugins.Draw(export=True, filename='data.geojson', position='topleft', draw_options=draw_options)
+    draw.add_to(m)
 
-        # center on Liberty Bell, add marker
-        m = folium.Map(location=[42.793553, 141.6958724], zoom_start=16)
-        folium.Marker(
-            [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
-        ).add_to(m)
-        # Leaflet.jsのDrawプラグインを追加
-        draw_options = {'polyline': True, 'rectangle': True, 'circle': True, 'marker': True, 'circlemarker': True}
-        draw = folium.plugins.Draw(export=True, filename='data.geojson', position='topleft', draw_options=draw_options)
-        draw.add_to(m)
-
-        # call to render Folium map in Streamlit
-        st_data = st_folium(m, width=725)
+    # call to render Folium map in Streamlit
+    st_data = st_folium(m, width=725)     
 
 with right:
     data = dict(st_data)
