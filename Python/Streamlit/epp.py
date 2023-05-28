b4c0eb313f046eda5e947954a194f4d0c918855f
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-05-28 12:07:51 +0900

    Update epp.py

--- Python/Streamlit/epp.py
+++ Python/Streamlit/epp.py
@@ -146,9 +146,9 @@ if uploaded_csvfile is not None:
     # DataFrameをサイドバーに表示
     st.session_state['df'] = df_new
     
-    selected_values = st.sidebar.st.multiselect("選択してください", df["newid"].unique())
+    selected_values = st.sidebar.multiselect("選択してください", df["newid"].unique())
 
-    st.sidebar.st.write("選択された値:", selected_values)
+    st.sidebar.write("選択された値:", selected_values)
 
 
 # call to render Folium map in Streamlit
