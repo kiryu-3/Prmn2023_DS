717d333bdb2a6c9225887ea783ab1c3762610eb3
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-05-29 11:09:04 +0900

    Update epp.py

--- Python/Streamlit/epp.py
+++ Python/Streamlit/epp.py
@@ -186,27 +186,27 @@ with st.sidebar:
         
     
     # 削除する図形のIDを入力するテキストボックスを表示
-    if len(st.session_state['draw_data']) != 0:
-        delete_shape_id = st.text_input("削除する図形のIDを入力してください")
-        # Deleteボタンがクリックされた場合
-        if st.button("Delete"):
-            if delete_shape_id:
-                try:
-                    delete_shape_id = int(delete_shape_id)
-                    if delete_shape_id > 0 and delete_shape_id <= len(st.session_state['draw_data']):
-                        # 削除対象の図形を特定
-                        delete_shape = st.session_state['draw_data'][delete_shape_id-1]
-                        # 図形をマップから削除
-                        for key, value in st.session_state['map']._children.items():
-                            if isinstance(value, folium.features.GeoJson) and value.data == delete_shape:
-                                del st.session_state['map']._children[key]
-                        # draw_dataから図形を削除
-                        st.session_state['draw_data'].remove(delete_shape)
-                        st.sidebar.success("図形を削除しました")
-                    else:
-                        st.sidebar.error("指定されたIDの図形は存在しません")
-                except:
-                    st.sidebar.error("自然数値を入力してください")
+#     if len(st.session_state['draw_data']) != 0:
+#         delete_shape_id = st.text_input("削除する図形のIDを入力してください")
+#         # Deleteボタンがクリックされた場合
+#         if st.button("Delete"):
+#             if delete_shape_id:
+#                 try:
+#                     delete_shape_id = int(delete_shape_id)
+#                     if delete_shape_id > 0 and delete_shape_id <= len(st.session_state['draw_data']):
+#                         # 削除対象の図形を特定
+#                         delete_shape = st.session_state['draw_data'][delete_shape_id-1]
+#                         # 図形をマップから削除
+#                         for key, value in st.session_state['map']._children.items():
+#                             if isinstance(value, folium.features.GeoJson) and value.data == delete_shape:
+#                                 del st.session_state['map']._children[key]
+#                         # draw_dataから図形を削除
+#                         st.session_state['draw_data'].remove(delete_shape)
+#                         st.sidebar.success("図形を削除しました")
+#                     else:
+#                         st.sidebar.error("指定されたIDの図形は存在しません")
+#                 except:
+#                     st.sidebar.error("自然数値を入力してください")
                     
 # call to render Folium map in Streamlit
 st_data = st_folium(st.session_state['map'], width=725)  
