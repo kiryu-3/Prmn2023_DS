cf194e149af90b0c3b15d42b668fc9dac2a7c277
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-05-30 09:00:31 +0900

    Update epp.py

--- Python/Streamlit/epp.py
+++ Python/Streamlit/epp.py
@@ -252,8 +252,7 @@ if len(st.session_state['draw_data']) != 0:
     if delete_shape_id:
     
         if st.button("Delete"):
-            try:
-                delete_shape_id = int(delete_shape_id)
+            delete_shape_id = int(delete_shape_id)
                 if delete_shape_id > 0 and delete_shape_id < len(st.session_state['draw_data']) + 1:
                     # 削除対象の図形を特定
                     delete_shape = st.session_state['draw_data'][delete_shape_id-1]
@@ -269,7 +268,24 @@ if len(st.session_state['draw_data']) != 0:
                     st.button("Do")
                 else:
                     st.error("指定されたIDの図形は存在しません")
-            except:
-                # st.write(st.session_state['draw_data'][int(delete_shape_id)-1])
-                st.error("自然数値を入力してください")
+#             try:
+#                 delete_shape_id = int(delete_shape_id)
+#                 if delete_shape_id > 0 and delete_shape_id < len(st.session_state['draw_data']) + 1:
+#                     # 削除対象の図形を特定
+#                     delete_shape = st.session_state['draw_data'][delete_shape_id-1]
+#                     st.write(delete_shape)
+#                     # 図形をマップから削除
+#                     for key, value in st.session_state['map']._children.items():
+#                         if isinstance(value, folium.features.GeoJson) and value.data == delete_shape:
+#                             del st.session_state['map']._children[key]
+#                     # draw_dataから図形を削除
+#                     # st.write(st.session_state['draw_data'])
+#                     st.session_state['draw_data'].remove(delete_shape)
+#                     # st.write(st.session_state['draw_data'])
+#                     st.button("Do")
+#                 else:
+#                     st.error("指定されたIDの図形は存在しません")
+#             except:
+#                 # st.write(st.session_state['draw_data'][int(delete_shape_id)-1])
+#                 st.error("自然数値を入力してください")
 
