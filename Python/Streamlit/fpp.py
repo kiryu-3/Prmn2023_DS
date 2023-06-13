7c72eade05c6bb261bb41da56becaa2e428e3584
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-06-13 11:48:59 +0900

    Update fpp.py

--- Python/Streamlit/fpp.py
+++ Python/Streamlit/fpp.py
@@ -236,7 +236,7 @@ except Exception as e:
 
 # 削除する図形のIDを入力するテキストボックスを表示
 if len(st.session_state['draw_data']) != 0:
-    delete_shape_id = st.text_input("削除する図形のIDを入力してください")
+    # delete_shape_id = st.text_input("削除する図形のIDを入力してください")
     multi_area = st.empty()
     delete_shape_id = multi_area.selectbox("選択してください", [""]
                                             + [str(value) for value in range(1, len(st.session_state['draw_data']) + 1)])
@@ -261,29 +261,4 @@ if len(st.session_state['draw_data']) != 0:
             for data in st.session_state['draw_data']:
                 tooltip_html = '<div style="font-size: 16px;">gateid：{}</div>'.format(st.session_state['draw_data'].index(data)+1)
                 folium.GeoJson(data, popup=folium.Popup(tooltip_html)).add_to(st.session_state['map'])
-                 
-            
-#             try:
-#                 delete_shape_id = int(delete_shape_id)
-#                 if delete_shape_id > 0 and delete_shape_id <= len(st.session_state['draw_data']):
-#                     # 削除対象の図形を特定
-#                     delete_shape = st.session_state['draw_data'][delete_shape_id - 1]
-#                     # 図形をマップから削除するためのキーを記録
-#                     keys_to_remove = []
-#                     for key, value in st.session_state['map']._children.items():
-#                         if isinstance(value, folium.features.GeoJson) and value.data == delete_shape:
-#                             keys_to_remove.append(key)
-#                     # マップから図形を削除
-#                     for key in keys_to_remove:
-#                         del st.session_state['map']._children[key]
-#                     # draw_dataから図形を削除
-#                     st.session_state['draw_data'].remove(delete_shape)
-#                     for data in st.session_state['draw_data']:
-#                         tooltip_html = '<div style="font-size: 16px;">gateid：{}</div>'.format(st.session_state['draw_data'].index(data)+1)
-#                         folium.GeoJson(data, popup=folium.Popup(tooltip_html)).add_to(st.session_state['map'])
-            
-
-#                 else:
-#                     st.error("指定されたIDの図形は存在しません")
-#             except:
-#                 st.error("自然数値を入力してください")
+                
