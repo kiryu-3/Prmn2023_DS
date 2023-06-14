94aa09abd8d80df54f76674fe6aa7af1365d5a76
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-06-14 10:56:13 +0900

    Update fpp.py

--- Python/Streamlit/fpp.py
+++ Python/Streamlit/fpp.py
@@ -232,7 +232,7 @@ except Exception as e:
 
 # st.subheader("地図の全描画データ")
 # st.write(data["all_drawings"])
-st.write(data["all_drawings"])
+# st.write(data["all_drawings"])
 st.write(st.session_state['draw_data'])  
 st.write(st_data)  
 
