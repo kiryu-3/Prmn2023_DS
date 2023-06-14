d759e87d5173e84d66bec073ef90504217a0f9a5
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-06-14 12:24:25 +0900

    Update fpp.py

--- Python/Streamlit/fpp.py
+++ Python/Streamlit/fpp.py
@@ -221,7 +221,7 @@ try:
                 st.write("yes")
                 data["all_drawings"][0]["geometry"]["type"] = "Polygon"
                 data["all_drawings"][0]["geometry"]["coordinates"] = data["last_circle_polygon"]["coordinates"]
-                st.session_state['draw_data'].append(data["all_drawings"])
+        st.session_state['draw_data'].append(data["all_drawings"])
 #         for idx in range(len(data["all_drawings"])):
 #             # data["all_drawings"][idx]["properties"] = str(idx+1)
 #             if data["last_circle_polygon"] is not None:
