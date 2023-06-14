6213cd53ca597f285ed4a82f9b9038953e68cdd2
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-06-14 18:54:00 +0900

    Update fpptest.py

--- Python/Streamlit/fpptest.py
+++ Python/Streamlit/fpptest.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def multi_file_uploader(label, key):
-    uploaded_files = st.file_uploader(label, key=key, accept_multiple_files=True)
+    uploaded_files = st.file_uploader(label, key=key, accept_multiple_files=True, type=["csv"])
     return uploaded_files
 
 # 使用例
@@ -10,4 +10,13 @@ uploaded_files = multi_file_uploader("複数のファイルを選択してくだ
 # 選択されたファイルを表示する例
 if uploaded_files:
     for file in uploaded_files:
-        st.write(file.name)
+        file_data = file.read()
+        # バイナリデータからPandas DataFrameを作成
+        df = pd.read_csv(io.BytesIO(file_data))
+        df.to_excel(buf := BytesIO(), index=False)
+        st.download_button(
+        "Download",
+        buf.getvalue(),
+        "sample.csv",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        )
