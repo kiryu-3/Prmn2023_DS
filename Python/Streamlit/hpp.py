32ff28ca9aae62fc4edebf42756a858df2ecb347
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-06-28 02:18:22 +0900

    Update hpp.py

--- Python/Streamlit/hpp.py
+++ Python/Streamlit/hpp.py
@@ -326,9 +326,9 @@ try:
 #             st.session_state['draw_data'].append(data["all_drawings"][idx])
         
         
-        # for sdata in st.session_state['draw_data']:
-        #     tooltip_html = '<div style="font-size: 16px;">gateid：{}</div>'.format(st.session_state['draw_data'].index(sdata)+1)
-        #     folium.GeoJson(sdata[0],tooltip=tooltip_html).add_to(st.session_state['map'])
+        for sdata in st.session_state['draw_data']:
+            tooltip_html = '<div style="font-size: 16px;">gateid：{}</div>'.format(st.session_state['draw_data'].index(sdata)+1)
+            folium.GeoJson(sdata[0],tooltip=tooltip_html).add_to(st.session_state['map'])
             # folium.GeoJson(sdata[0], popup=folium.Popup(tooltip_html)).add_to(st.session_state['map'])
             
             
