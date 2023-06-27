537a9392109ab52202b4550b8457eb8f5976d5fa
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-06-27 22:39:32 +0900

    Update hpp.py

--- Python/Streamlit/hpp.py
+++ Python/Streamlit/hpp.py
@@ -179,7 +179,7 @@ with st.sidebar:
                             st.session_state['kiseki_data'].append([[df2.iloc[i, 3], df2.iloc[i, 2]],
                                                     [df2.iloc[i + 1, 3], df2.iloc[i + 1, 2]]])
                     # tab3.write(st.session_state['kiseki_data'])
-                    tab3.write(df2)
+                    # tab5.write(df2)
                     tab3.write(line_features)
                     line_geojson = {'type': 'FeatureCollection', 'features': line_features}
                     # 線のジオJSONを追加
