a071aad17933215c069f68ad9c2f950beaa8a56d
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-06-28 09:54:10 +0900

    Update hpp.py

--- Python/Streamlit/hpp.py
+++ Python/Streamlit/hpp.py
@@ -383,15 +383,15 @@ if len(st.session_state['draw_data']) != 0:
     # 最初の要素のみを取得してst.session_state['gate_data']に追加
     sdata = st.session_state['draw_data'][0]
     st.session_state['gate_data'].append(sdata[0]["geometry"]["coordinates"])
-    tab4.write(len(st.session_state['gate_data']))
-    tab4.write(st.session_state['gate_data'])
+    # tab4.write(len(st.session_state['gate_data']))
+    # tab4.write(st.session_state['gate_data'])
     tab4.write(len(sdata[0]["geometry"]["coordinates"]))
     
     for _ in range(len(st.session_state['gate_data'])):
         st.session_state['tuuka_list'].append("0")
-    tab4.write(len(st.session_state['draw_data'])-1)
-    tab4.write(len(st.session_state['gate_data']))
-    tab4.write(st.session_state['tuuka_list'])
+    # tab4.write(len(st.session_state['draw_data'])-1)
+    # tab4.write(len(st.session_state['gate_data']))
+    # tab4.write(st.session_state['tuuka_list'])
     # tuuka_list = [0 for _ in range(len(st.session_state['draw_data'])-1)]
    
     if len(st.session_state['df']) != 0:      
