f34b5e2b22447465a90f540154dcb36dc754adff
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-06-29 23:29:32 +0900

    Update hpp.py

--- Python/Streamlit/hpp.py
+++ Python/Streamlit/hpp.py
@@ -454,7 +454,7 @@ if len(st.session_state['draw_data']) != 0:
             popup_html = '<div style="font-size: 16px;">通過人数：{}人</div>'.format(st.session_state['tuuka_list'][idx])
             folium.GeoJson(sdata,tooltip=tooltip_html,popup=folium.Popup(popup_html)).add_to(st.session_state['map'])
         else:
-            folium.GeoJson(sdata[0],tooltip=tooltip_html).add_to(st.session_state['map'])
+            folium.GeoJson(sdata,tooltip=tooltip_html).add_to(st.session_state['map'])
         if len(st.session_state['df']) != 0:
             popup_html = '<div style="font-size: 16px;">通過人数：{}人</div>'.format(st.session_state['draw_data'].index(sdata)+1)
             folium.GeoJson(sdata,popup=folium.Popup(popup_html)).add_to(tuuka_list[idx])
