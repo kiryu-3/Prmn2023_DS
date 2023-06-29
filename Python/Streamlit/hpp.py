f59a433a4e09ccc6deaa2cb98b3902b747577c02
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-06-30 01:32:41 +0900

    Update hpp.py

--- Python/Streamlit/hpp.py
+++ Python/Streamlit/hpp.py
@@ -313,7 +313,7 @@ with st.sidebar:
                 if len(st.session_state['draw_data']) != 0:
                     for idx, sdata in enumerate(st.session_state['draw_data']):
                         tooltip_html = '<div style="font-size: 16px;">gateid：{}</div>'.format(st.session_state['draw_data'].index(sdata)+1)
-                        folium.GeoJson(sdata).add_to(st.session_state['map'])
+                        folium.GeoJson(sdata[0]).add_to(st.session_state['map'])
                     
 # call to render Folium map in Streamlit
 st_data = st_folium(st.session_state['map'], width=725)  
