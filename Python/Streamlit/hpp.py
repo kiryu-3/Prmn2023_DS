5d82a1a3978e161a211899aa784ec62420279007
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-06-30 01:34:38 +0900

    Update hpp.py

--- Python/Streamlit/hpp.py
+++ Python/Streamlit/hpp.py
@@ -275,7 +275,7 @@ with st.sidebar:
                 if len(st.session_state['draw_data']) != 0:
                     for idx, sdata in enumerate(st.session_state['draw_data']):
                         tooltip_html = '<div style="font-size: 16px;">gateid：{}</div>'.format(st.session_state['draw_data'].index(sdata)+1)
-                        folium.GeoJson(sdata[0]).add_to(st.session_state['map'])
+                        folium.GeoJson(sdata,tooltip=tooltip_html).add_to(st.session_state['map'])
                     
             else:
                 df = pd.DataFrame()
@@ -313,7 +313,7 @@ with st.sidebar:
                 if len(st.session_state['draw_data']) != 0:
                     for idx, sdata in enumerate(st.session_state['draw_data']):
                         tooltip_html = '<div style="font-size: 16px;">gateid：{}</div>'.format(st.session_state['draw_data'].index(sdata)+1)
-                        folium.GeoJson(sdata[0]).add_to(st.session_state['map'])
+                        folium.GeoJson(sdata,tooltip=tooltip_html).add_to(st.session_state['map'])
                     
 # call to render Folium map in Streamlit
 st_data = st_folium(st.session_state['map'], width=725)  
