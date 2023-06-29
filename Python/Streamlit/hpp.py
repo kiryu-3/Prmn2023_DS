04ea4345742fc16da92450e977c48d2b79a98e52
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-06-29 21:48:17 +0900

    Update hpp.py

--- Python/Streamlit/hpp.py
+++ Python/Streamlit/hpp.py
@@ -343,6 +343,7 @@ st.subheader("地図の全描画データ")
 # st.write(data["all_drawings"])
 st.write(st.session_state['draw_data'])
 st.write(type(st.session_state['draw_data']))
+st.write(type(st.session_state['draw_data'][0]))
 
 
 def are_lines_intersecting(line1, line2):
