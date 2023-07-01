5df34af008d0009a3cb0badd904872745695c805
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-07-01 17:31:34 +0900

    Update jpp.py

--- Python/Streamlit/jpp.py
+++ Python/Streamlit/jpp.py
@@ -170,6 +170,10 @@ try:
                 data["all_drawings"][0]["properties"]["center"] = center_dict
 
         st.session_state['draw_data'].append(data["all_drawings"])
+        append_list = list()
+        for _ in range(len(st.session_state['gate_data'])):
+            append_list.append(0)
+        st.session_state['tuuka_list'] = append_list
         # st.session_state['draw_data'].append(data["all_drawings"])
 #         for idx in range(len(data["all_drawings"])):
 #             # data["all_drawings"][idx]["properties"] = str(idx+1)
