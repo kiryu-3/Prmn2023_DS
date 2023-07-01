a7178854048219bd139732e5a0ed36ed0fde3f0d
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-07-01 17:46:23 +0900

    Update jpp.py

--- Python/Streamlit/jpp.py
+++ Python/Streamlit/jpp.py
@@ -200,6 +200,7 @@ except Exception as e:
 st.subheader("地図の全描画データ")
 # st.write(data["all_drawings"])
 st.write(st.session_state['draw_data'])
+st.write(len(st.session_state['draw_data'])
 
 with st.sidebar:
     # タブ
