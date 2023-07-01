599b858e52dc0db626f7b652baf6715130912cf5
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-07-01 21:59:33 +0900

    Update jpp.py

--- Python/Streamlit/jpp.py
+++ Python/Streamlit/jpp.py
@@ -117,7 +117,7 @@ def kousa():
     
         # IDでループ
         for key, values in st.session_state['kiseki_data'].items():
-            # values[0]["座標"][0][0]
+            #values[0]["座標"][0][0]
             for value in values:
                     line1 = [(value["座標"][0][0], value["座標"][0][1]),
                              (value["座標"][1][0], value["座標"][1][1])]
