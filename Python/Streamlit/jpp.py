a7a6ce3f36aaddb6e9f890eb1962bd8ced625e53
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-07-02 13:44:45 +0900

    Update jpp.py

--- Python/Streamlit/jpp.py
+++ Python/Streamlit/jpp.py
@@ -283,6 +283,8 @@ with st.sidebar:
         
             # df.sort_values(by=[df.columns[1]], inplace=True)
             kiseki = tab4.checkbox(label='軌跡の表示', key='kiseki2')
+            if kiseki:
+                st.session_state['kasa'] = True
             list2 = list()
             for i, row in df.iterrows():
                 if row.iloc[0] not in list2:
