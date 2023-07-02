c472188b68ac2124f08311f8635e1747ac04536a
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-07-02 20:14:57 +0900

    Update jpp.py

--- Python/Streamlit/jpp.py
+++ Python/Streamlit/jpp.py
@@ -485,9 +485,9 @@ try:
                     kousa()
                     # st.session_state['count'] += 1
                     popup_html = '<div style="font-size: 16px;">通過人数：{}人</div>'.format(st.session_state['tuuka_list'][idx])
-                    folium.GeoJson(sdata[0], tooltip=tooltip_html, popup=folium.Popup(popup_html)).add_to(st.session_state['map'])
+                    folium.GeoJson(sdata, tooltip=tooltip_html, popup=folium.Popup(popup_html)).add_to(st.session_state['map'])
                 else:
-                    folium.GeoJson(sdata[0], tooltip=tooltip_html).add_to(st.session_state['map'])
+                    folium.GeoJson(sdata, tooltip=tooltip_html).add_to(st.session_state['map'])
             raise st.experimental_rerun()
             
     else:
