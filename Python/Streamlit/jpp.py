294d6606af47071a66b46b89edf6cf94e110a2b5
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-07-03 11:14:33 +0900

    Update jpp.py

--- Python/Streamlit/jpp.py
+++ Python/Streamlit/jpp.py
@@ -593,6 +593,9 @@ with st.sidebar:
     with tab4:
         if len(st.session_state['df']) != 0:
             st.checkbox(label='軌跡の表示', key='kiseki_flag', on_change=kiseki_draw)
-            st.write(st.session_state['line1'])
-            st.write(st.session_state['line2'])
+            try:
+                st.write(st.session_state['line1'])
+                st.write(st.session_state['line2'])
+            except Exception as e:
+                pass
             # st.write(st.session_state['kiseki_data'])
