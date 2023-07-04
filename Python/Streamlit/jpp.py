eaf24d05b94688ebd364c8ce53bd5d89e14be302
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-07-04 16:09:03 +0900

    Update jpp.py

--- Python/Streamlit/jpp.py
+++ Python/Streamlit/jpp.py
@@ -221,6 +221,7 @@ def upload_csv():
 def select_data():
     # プロット・軌跡を描画するデータの選択
     selected_values = st.session_state["select_data_id"]
+    selected_values = [str(value) for value in selected_values]
     # 選択されていない場合はそのままのデータ
     if len(selected_values) == 0:
         st.session_state['sorted_df'] = st.session_state['df']
@@ -239,7 +240,7 @@ def select_data():
 
         # ユニークなIDのリスト
         # リストの全ての要素を文字列型に変換する
-        list2 = [str(value) for value in selected_values]
+        list2 = selected_values
         
         # 描画するプロットデータ
         features = []
