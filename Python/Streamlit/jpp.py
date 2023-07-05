2bb721d349725d023f98825fe91a9d3500fea63d
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-07-05 11:48:21 +0900

    Update jpp.py

--- Python/Streamlit/jpp.py
+++ Python/Streamlit/jpp.py
@@ -678,10 +678,17 @@ try:
                     folium.GeoJson(sdata, tooltip=tooltip_html, popup=folium.Popup(popup_html)).add_to(st.session_state['map'])
                 else:
                     folium.GeoJson(sdata, tooltip=tooltip_html).add_to(st.session_state['map'])
+
+            
                     
 
             # 最初の要素のみを取得してst.session_state['gate_data']に追加
             st.session_state['gate_data'] = gate_append_list
+            
+            # 線のジオJSONを追加
+            folium.GeoJson(st.session_state["line_geojson"], name='線の表示/非表示',
+                       style_function=lambda x: {"weight": 2, "opacity": 1}).add_to(st.session_state['map'])
+    
             # raise st.experimental_rerun()
     else:
         # st.write(e)
