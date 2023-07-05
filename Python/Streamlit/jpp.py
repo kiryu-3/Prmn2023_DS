d0aa8691934d502f4f94c2606945658b71990f35
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-07-05 12:02:32 +0900

    Update jpp.py

--- Python/Streamlit/jpp.py
+++ Python/Streamlit/jpp.py
@@ -724,7 +724,7 @@ with st.sidebar:
        
     with tab3:
         if len(st.session_state['draw_data']) != 0:
-            # st.write(st.session_state['gate_data'])
+            st.write(st.session_state['gate_data'])
             st.selectbox("表示したい図形のIDを選択してください", [""]+ [str(value) for value in range(1, len(st.session_state['gate_data']) + 1)],
                          key="select_shape_id",
                          on_change=select_shape)
@@ -743,8 +743,8 @@ with st.sidebar:
         if len(st.session_state['df']) != 0:
             st.checkbox(label='軌跡の表示', key='kiseki_flag', on_change=kiseki_draw)
 
-            # st.write(st.session_state['kiseki_data'])
-            st.write(st.session_state['tuuka_list'])
+            st.write(st.session_state['kiseki_data'])
+            # st.write(st.session_state['tuuka_list'])
             # st.subheader("count")
             # st.write(st.session_state['count'])
             # st.subheader("ingate_count")
