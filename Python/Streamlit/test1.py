5a9d0a4535ee52bc5bd692dd69487e8b23a1f288
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-07-09 02:41:50 +0900

    Update test1.py

--- Python/Streamlit/test1.py
+++ Python/Streamlit/test1.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pylatex import Document, Section, Subsection, Command
+import subprocess
 
 # ファイルのアップロード
 uploaded_file = st.file_uploader("Upload LaTeX File", type="tex")
@@ -8,15 +8,14 @@ if uploaded_file is not None:
     # テキストファイルの内容を取得
     tex_content = uploaded_file.getvalue().decode()
 
-    # LaTeXドキュメントを作成
-    doc = Document()
-    doc.append(Command('title', 'Generated PDF'))
-    doc.append(Command('author', 'User'))
-    doc.append(Command('date', 'Today'))
-    doc.append(tex_content)
+    # LaTeXファイルを一時ファイルとして保存
+    temp_file_path = "/tmp/temp.tex"
+    with open(temp_file_path, "w") as f:
+        f.write(tex_content)
 
-    # PDFに変換
-    pdf_bytes = doc.generate_pdf()
+    # pdflatexコマンドを使用してPDFに変換
+    subprocess.run(["pdflatex", "-interaction=batchmode", temp_file_path])
 
-    # 変換後のPDFをダウンロード
-    st.download_button("Download PDF", pdf_bytes, file_name='output.pdf', mime='application/pdf')
+    # 変換後のPDFを表示
+    pdf_file_path = temp_file_path.replace(".tex", ".pdf")
+    st.download_button("Download PDF", pdf_file_path, file_name="output.pdf", mime="application/pdf")
