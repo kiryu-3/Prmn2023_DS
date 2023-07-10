8222fbbf30399c72d70a06b2bf1b6f3ca862c2fc
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-07-10 15:38:08 +0900

    Update lpp.py

--- Python/Streamlit/lpp.py
+++ Python/Streamlit/lpp.py
@@ -222,7 +222,7 @@ def upload_csv():
                        st.session_state['non_cross_judge_count'] += 1
             
             st.session_state['count'] += 1
-            popup_html = '<div style="font-size: 16px;">通過人数：{}人</div>'.format(st.session_state['tuuka_list'][idx])
+            popup_html = '<div style="font-size: 16px; width: 200px; height: 200px;">通過人数：{}人</div>'.format(st.session_state['tuuka_list'][idx])
             folium.GeoJson(sdata, tooltip=tooltip_html, popup=folium.Popup(popup_html)).add_to(st.session_state['map'])
                 
             
@@ -394,9 +394,9 @@ def select_data():
                    st.session_state['non_cross_judge_count'] += 1
         
         st.session_state['count'] += 1
-        popup_html = '<div style="font-size: 16px;">通過人数：{}人</div>'.format(st.session_state['tuuka_list'][idx])
+        popup_html = '<div style="font-size: 16px; width: 200px; height: 200px;">通過人数：{}人</div>'.format(st.session_state['tuuka_list'][idx])
         folium.GeoJson(sdata, tooltip=tooltip_html, popup=folium.Popup(popup_html)).add_to(st.session_state['map'])
-
+        
     # 軌跡の追加
     if st.session_state['kiseki_flag']:
         # 線のジオJSONを追加
@@ -493,7 +493,7 @@ def kiseki_draw():
                        st.session_state['non_cross_judge_count'] += 1
             
             st.session_state['count'] += 1
-            popup_html = '<div style="font-size: 16px;">通過人数：{}人</div>'.format(st.session_state['tuuka_list'][idx])
+            popup_html = '<div style="font-size: 16px; width: 200px; height: 200px;">通過人数：{}人</div>'.format(st.session_state['tuuka_list'][idx])
             folium.GeoJson(sdata, tooltip=tooltip_html, popup=folium.Popup(popup_html)).add_to(st.session_state['map'])
                 
         
@@ -571,7 +571,7 @@ def delete_shape():
                        st.session_state['non_cross_judge_count'] += 1
             
             st.session_state['count'] += 1
-            popup_html = '<div style="font-size: 16px;">通過人数：{}人</div>'.format(st.session_state['tuuka_list'][idx])
+            popup_html = '<div style="font-size: 16px; width: 200px; height: 200px;">通過人数：{}人</div>'.format(st.session_state['tuuka_list'][idx])
             folium.GeoJson(sdata, tooltip=tooltip_html, popup=folium.Popup(popup_html)).add_to(st.session_state['map'])
             
 # def are_lines_intersecting(line1, line2):
@@ -838,7 +838,7 @@ try:
                                st.session_state['non_cross_judge_count'] += 1
                     
                     st.session_state['count'] += 1
-                    popup_html = '<div style="font-size: 16px;">通過人数：{}人</div>'.format(st.session_state['tuuka_list'][idx])
+                    popup_html = '<div style="font-size: 16px; width: 200px; height: 200px;">通過人数：{}人</div>'.format(st.session_state['tuuka_list'][idx])
                     folium.GeoJson(sdata, tooltip=tooltip_html, popup=folium.Popup(popup_html)).add_to(st.session_state['map'])
                     # st.experimental_rerun()
                 else:
