fcbfcbe277bf00294e2f20fc9726caf39c832a72
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-07-18 16:50:08 +0900

    Update tetest3.py

--- Python/Streamlit/tetest3.py
+++ Python/Streamlit/tetest3.py
@@ -643,7 +643,7 @@ try:
                     # 図形IDを表示するツールチップを設定
                     tooltip_html = '<div style="font-size: 16px;">gateid：{}</div>'.format(idx + 1)
                     # 通過人数を表示するポップアップを指定
-                    popup_html = '<div style="font-size: 16px; font-weight: bold; width: 110px; height: 20px;  color: #27b9cc;">通過人数：{}人</div>'.format(len(st.session_state['tuuka_list'][idx1])
+                    popup_html = '<div style="font-size: 16px; font-weight: bold; width: 110px; height: 20px;  color: #27b9cc;">通過人数：{}人</div>'.format(len(st.session_state['tuuka_list'][idx1]))
                     folium.GeoJson(sdata, tooltip=tooltip_html, popup=folium.Popup(popup_html)).add_to(st.session_state['map'])
                 else: 
                     # 図形IDを表示するツールチップを設定
