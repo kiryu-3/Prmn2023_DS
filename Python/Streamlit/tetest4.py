bd93763284e17c81ca19eb75aa91ed3ba8a80c61
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-07-26 18:56:39 +0900

    Update tetest4.py

--- Python/Streamlit/tetest4.py
+++ Python/Streamlit/tetest4.py
@@ -865,7 +865,7 @@ if len(st.session_state['df']) != 0 and len(st.session_state['gate_data']):
         # グラフを表示
         st.plotly_chart(fig)
         st.write(max_y_value)
-        st.write(st.session_state['graph_data'])
+        st.write(len(st.session_state['graph_data']))
                 
 with st.sidebar:
     # タブ
