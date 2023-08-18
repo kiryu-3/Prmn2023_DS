84b937d100c6171df2e7cabd5f9d534f314fac46
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-08-19 00:53:15 +0900

    Update tttttt.py

--- Python/Streamlit/tttttt.py
+++ Python/Streamlit/tttttt.py
@@ -1,27 +1,34 @@
 import streamlit as st
+from streamlit_folium import st_folium
 import folium
-from streamlit_folium import folium_static
-import copy
 
-# 初期の中心座標を設定
-initial_center = {"lat": 35.6895, "lng": 139.6917}
-if "center" not in st.session_state:
-    st.session_state.center = initial_center
+# Streamlitアプリケーションの設定
+st.set_page_config(page_title="Folium Map Info Example")
 
-# 地図を作成
-if not hasattr(st.session_state, "draw_added"):
-    draw_options = {'polyline': True, 'rectangle': True, 'circle': True, 'marker': False, 'circlemarker': False}
-    draw = folium.plugins.Draw(export=False, position='topleft', draw_options=draw_options)
-    st.session_state.map = folium.Map(location=[st.session_state.center["lat"], st.session_state.center["lng"]], zoom_start=16)
-    draw.add_to(st.session_state.map)
-    st.session_state.draw_added = True
-else:
-    st.session_state.map = folium.Map(location=[st.session_state.center["lat"], st.session_state.center["lng"]], zoom_start=16)
+# セッション状態を初期化
+if "map_info" not in st.session_state:
+    st.session_state.map_info = {
+        "zoom": 10,
+        "latitude": 37.7749,
+        "longitude": -122.4194
+    }
 
-# 地図をStreamlitコンポーネントに追加
-st_data = folium_static(st.session_state.map, width=725)
+# ユーザーが設定した情報を表示するセクション
+st.sidebar.header("Map Information")
 
-# 地図の中心座標を保存
-data = copy.deepcopy(dict(st_data))
-st.session_state.center["lat"] = data["center"]["lat"]
-st.session_state.center["lng"] = data["center"]["lng"]
+# ズームレベルを更新
+new_zoom = st.sidebar.slider("Zoom Level", min_value=1, max_value=20, value=st.session_state.map_info["zoom"])
+st.session_state.map_info["zoom"] = new_zoom
+
+# 中心座標（緯度と経度）を更新
+new_latitude = st.sidebar.number_input("Latitude", value=st.session_state.map_info["latitude"])
+new_longitude = st.sidebar.number_input("Longitude", value=st.session_state.map_info["longitude"])
+st.session_state.map_info["latitude"] = new_latitude
+st.session_state.map_info["longitude"] = new_longitude
+
+# Folium Mapオブジェクトを作成
+m = folium.Map(location=[st.session_state.map_info["latitude"], st.session_state.map_info["longitude"]],
+               zoom_start=st.session_state.map_info["zoom"])
+
+# StreamlitコンポーネントにFolium Mapオブジェクトを埋め込む
+st_folium(m)
