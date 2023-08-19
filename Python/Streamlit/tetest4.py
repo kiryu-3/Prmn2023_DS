c95576265a77f50e3418095d9b3e5347f3cbf67f
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-08-20 01:11:40 +0900

    Update tetest4.py

--- Python/Streamlit/tetest4.py
+++ Python/Streamlit/tetest4.py
@@ -709,7 +709,7 @@ def ingate(plot_point, gate_polygon):
 
 
 # 表示する地図
-st_data = st_folium(st.session_state['map'], width=725)
+st_data = st_folium(st.session_state['map'], width=800, height=800, zoom_start=16)
 
 # 地図のデータをコピー
 data = copy.deepcopy(dict(st_data))
