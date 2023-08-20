770fdd262fb19d22f81806d842b89e6d77a10b77
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-08-21 00:30:26 +0900

    Update skmob1.py

--- Python/Streamlit/skmob1.py
+++ Python/Streamlit/skmob1.py
@@ -3,35 +3,26 @@ import folium
 from streamlit_folium import folium_static
 import skmob
 from datetime import datetime
-
-# ダミーのデータと関数（実際のデータや関数に置き換える必要があります）
-latitude = 37.7749
-longitude = -122.4194
-path_coordinates = [(37.7749, -122.4194), (37.774, -122.419), (37.773, -122.418)]
-def filter_data_by_date(selected_date):
-    # ダミーのデータフィルタリング
-    return []
-
-def calculate_total_distance(data):
-    # ダミーの統計情報計算
-    return 10.0
+import pandas as pd
 
 # Streamlitアプリケーションの設定
 st.title("移動データ可視化アプリ")
 
-# 地図の初期設定
-m = folium.Map(location=[latitude, longitude], zoom_start=12)
+# CSVファイルをアップロード
+uploaded_file = st.file_uploader("CSVファイルをアップロード", type=["csv"])
 
-# 移動経路のプロット（streamlit-foliumを使用）
-folium.PolyLine(locations=path_coordinates, color='blue').add_to(m)
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
 
-# 地図の表示
-folium_static(m)
+    # scikit-mobilityのTrajDataFrameを作成
+    traj_df = skmob.TrajDataFrame(df, timestamp=True)
 
-# ユーザーインタラクション
-selected_date = st.date_input("表示する日付を選択", datetime.today())
-filtered_data = filter_data_by_date(selected_date)
+    # 地図の初期設定
+    m = folium.Map(location=[traj_df["lat"].mean(), traj_df["lng"].mean()], zoom_start=12)
 
-# 統計情報の表示（scikit-mobilityを使用）
-total_distance = calculate_total_distance(filtered_data)
-st.write(f"選択した日付の合計移動距離: {total_distance} km")
+    # 移動経路のプロット（streamlit-foliumを使用）
+    for _, row in traj_df.iterrows():
+        folium.CircleMarker(location=(row["lat"], row["lng"]), radius=5, color='blue').add_to(m)
+
+    # 地図の表示
+    folium_static(m)
