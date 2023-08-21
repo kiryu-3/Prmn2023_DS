619fc221a57a0e52abb5d6026329152b2a74e379
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-08-22 02:36:09 +0900

    Update tetest5.py

--- Python/Streamlit/tetest5.py
+++ Python/Streamlit/tetest5.py
@@ -178,10 +178,10 @@ def line_features_maker(list2, kiseki):
             #     }
             #     line_features.append(line_feature)
 
-                if kiseki:
-                    # 軌跡データをセッションの状態に保存
-                    st.session_state['kiseki_data'][str(itr)].append({'座標': [coords[i], coords[i + 1]],
-                                                                      '日時': times[i]})
+            if kiseki:
+                # 軌跡データをセッションの状態に保存
+                st.session_state['kiseki_data'][str(itr)].append({'座標': [coords[i], coords[i + 1]],
+                                                                  '日時': times[i]})
     return line_features
 
 def polylines_maker():
