7d2af4d5a32821df8feb4d7c853b518cebf35d58
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-09-07 23:20:09 +0900

    Update tetest5.py

--- Python/Streamlit/tetest5.py
+++ Python/Streamlit/tetest5.py
@@ -1040,5 +1040,5 @@ with st.sidebar:
     with tab4:
         if len(st.session_state['df']) != 0:
             st.checkbox(label='軌跡の表示', key='kiseki_flag', on_change=kiseki_draw)
-        st.write(st.session_state['df'].head())    
-        st.write(st.session_state['sorted_df'].head())
+        st.write("test")    
+        st.write(st.session_state['sorted_df'])
