2d4697e7c1d19d1d4945e5f2cbbff4d180d92b3b
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-09-07 22:59:47 +0900

    Update tetest5.py

--- Python/Streamlit/tetest5.py
+++ Python/Streamlit/tetest5.py
@@ -252,7 +252,7 @@ def upload_csv():
 
         st.session_state['kiseki_data'] = {str(itr): [] for itr in unique_values}
 
-        features = features_maker()
+        # features = features_maker()
         line_features_maker(True)
 
         # プロットデータをまとめる
