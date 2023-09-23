19628045131ab20fbe3e2e1bea9bc58404203f95
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-09-23 12:24:34 +0900

    Update CSV_filter3.py

--- Python/Streamlit/CSV_filter3.py
+++ Python/Streamlit/CSV_filter3.py
@@ -70,7 +70,7 @@ def number_widget(df, column, ss_name):
         notna_df = notna_df[notna_df[f'{column}_numeric'].notna()]
         
         if (notna_df[f'{column}_numeric'] == notna_df[f'{column}_numeric'].astype(int)).all():
-            st.session_state["uploaded_df"] = st.session_state["uploaded_df"][column].astype(int)
+            st.session_state["uploaded_df"][column] = pd.to_numeric(st.session_state["uploaded_df"][column], errors='coerce').fillna(st.session_state["uploaded_df"][column])
             st.session_state["uploaded_df"] = st.session_state["uploaded_df"][column].astype("object")
             return (df, 'int')  # すべての値が整数に変換可能な場合は整数型と判定
         else:
