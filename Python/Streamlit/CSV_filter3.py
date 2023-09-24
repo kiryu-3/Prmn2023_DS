7a3d3d3582f33c7c8e4434f2272e1f1e6ca4a9ac
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-09-25 00:37:23 +0900

    Update CSV_filter3.py

--- Python/Streamlit/CSV_filter3.py
+++ Python/Streamlit/CSV_filter3.py
@@ -228,15 +228,15 @@ def datetime_widget(df, column, ss_name):
 def text_widget(df, column, ss_name):
     temp_df = df.dropna(subset=[column])
     options = list(temp_df[column].unique())
-    # try:
-    #     if all(value.isdigit() for value in options):
-    #         options = [int(value) for value in options]
-    #         options = [str(value) for value in options]
-    # except:
-    #     st.write(column)
-    if all(value.isdigit() for value in options):
-        options = [int(value) for value in options]
-        options = [str(value) for value in options]
+    try:
+        if all(value.isdigit() for value in options):
+            options = [int(value) for value in options]
+            options = [str(value) for value in options]
+    except:
+        st.write(column)
+    # if all(value.isdigit() for value in options):
+    #     options = [int(value) for value in options]
+    #     options = [str(value) for value in options]
     # if temp_df[column].apply(is_integer).sum() == len(temp_df[column]):
     #     temp_df[column] = temp_df[column].astype(int) 
     #     temp_df[column] = temp_df[column].astype("object") 
