6f6caa21dfc730c909228bd9ec978eecd015eb4e
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-09-25 18:47:55 +0900

    Update CSV_filter3.py

--- Python/Streamlit/CSV_filter3.py
+++ Python/Streamlit/CSV_filter3.py
@@ -506,7 +506,7 @@ if st.session_state["upload_csvfile"] is not None:
         key="download_name"
     )
     
-    df = st.session_state["all_df"][st.session_state["filtered_columns"]].copy()
+    df = st.session_state["all_df"].copy()
     
     create_data = st.session_state["column_data"]
     df, all_widgets = create_widgets(df, create_data)
