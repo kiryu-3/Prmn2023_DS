e317e7b27a5a5c53cbc4a207a8ecb8a4436bd8f0
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-09-25 11:01:15 +0900

    Update CSV_filter3.py

--- Python/Streamlit/CSV_filter3.py
+++ Python/Streamlit/CSV_filter3.py
@@ -78,16 +78,16 @@ def number_widget(df, column, ss_name):
 
     if temp_df[column].apply(is_integer).sum() == len(temp_df[column]):
         df[f'{column}_numeric'] = pd.to_numeric(df[column], errors="coerce")
-        temp_df[f'{column}_numeric'] = temp_df[column].copy()
-        temp_df = temp_df.astype({f'{column}_numeric': float})
-        # temp_df[f'{column}_numeric'] = pd.to_numeric(temp_df[column], errors="coerce")
+        # temp_df[f'{column}_numeric'] = temp_df[column].copy()
+        # temp_df = temp_df.astype({f'{column}_numeric': float})
+        temp_df[f'{column}_numeric'] = pd.to_numeric(temp_df[column], errors="coerce")
         max = int(temp_df[f'{column}_numeric'].max())
         min = int(temp_df[f'{column}_numeric'].min())
     else:
         df[f'{column}_numeric'] = pd.to_numeric(df[column], errors="coerce")
-        temp_df[f'{column}_numeric'] = temp_df[column].copy()
-        temp_df = temp_df.astype({f'{column}_numeric': float})
-        # temp_df[f'{column}_numeric'] = pd.to_numeric(temp_df[column], errors="coerce")
+        # temp_df[f'{column}_numeric'] = temp_df[column].copy()
+        # temp_df = temp_df.astype({f'{column}_numeric': float})
+        temp_df[f'{column}_numeric'] = pd.to_numeric(temp_df[column], errors="coerce")
         max = float(temp_df[f'{column}_numeric'].max())
         min = float(temp_df[f'{column}_numeric'].min())
     
@@ -105,9 +105,9 @@ def datetime_widget(df, column, ss_name):
     # カラムを日付型に変換
     
     df[f'{column}_datetime'] = pd.to_datetime(df[column], errors='coerce')
-    temp_df[f'{column}_datetime'] = temp_df[column].copy()
-    temp_df = temp_df.astype({f'{column}_datetime': datetime})
-    # temp_df[f'{column}_datetime'] = pd.to_datetime(temp_df[column], errors="coerce")
+    # temp_df[f'{column}_datetime'] = temp_df[column].copy()
+    # temp_df = temp_df.astype({f'{column}_datetime': datetime})
+    temp_df[f'{column}_datetime'] = pd.to_datetime(temp_df[column], errors="coerce")
     start_date = df[f'{column}_datetime'].min()
     end_date = df[f'{column}_datetime'].max()
     first_date = start_date.to_pydatetime()
