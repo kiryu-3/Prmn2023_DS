d90f9218c148b9b2f08ef8087657b8d931f9b386
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-09-26 03:24:09 +0900

    Update CSV_filter3.py

--- Python/Streamlit/CSV_filter3.py
+++ Python/Streamlit/CSV_filter3.py
@@ -524,7 +524,7 @@ if st.session_state["upload_csvfile"] is not None:
     
     create_data = st.session_state["column_data"]
     all_widgets = create_widgets(df, create_data)
-    # st.write(df)
+    st.write(create_data)
     show_df = filter_df(df, all_widgets)
     
     for column in show_df[st.session_state["filtered_columns"]].columns:
