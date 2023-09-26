f0e7827b9f8e17a7e79038c42176c11b8853d5e5
Kaoru Segawa <84606676+kiryu-3@users.noreply.github.com>
2023-09-26 14:51:10 +0900

    Update CSV_filter.py

--- Python/Streamlit/CSV_filter.py
+++ Python/Streamlit/CSV_filter.py
@@ -37,6 +37,45 @@ st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 st.title("CSV Filters")
 
+def upload_csv():
+  # csvがアップロードされたとき
+  if st.session_state['upload_csvfile'] is not None:
+      # アップロードされたファイルデータを読み込む
+      file_data = st.session_state['upload_csvfile'].read()
+      # バイナリデータからPandas DataFrameを作成
+      try:
+          df = pd.read_csv(io.BytesIO(file_data), encoding="utf-8", engine="python")
+          st.session_state["ja"] = False
+      except UnicodeDecodeError:
+          # UTF-8で読み取れない場合はShift-JISエンコーディングで再試行
+          df = pd.read_csv(io.BytesIO(file_data), encoding="shift-jis", engine="python")
+          st.session_state["ja"] = True
+          
+      # カラムの型を自動で適切に変換
+      df = df.infer_objects() 
+      try:
+          for column in df.columns:
+              df[column] = df[column].astype(pd.Int64Dtype(), errors='ignore')
+      except:
+          pass
+
+      df = df.applymap(lambda x: str(x) if not pd.isnull(x) else x)
+      st.session_state["uploaded_df"] = df.copy()
+      st.session_state["all_df"] = df.copy()
+      create_data = decide_dtypes(df)
+      st.session_state["all_df"] = st.session_state["all_df"].applymap(lambda x: str(x) if not pd.isnull(x) else x)
+      
+      
+      st.session_state["filtered_columns"] = st.session_state["uploaded_df"].columns
+
+      st.session_state["column_data"] = decide_dtypes(df)
+
+  else:
+      st.session_state["uploaded_df"] = pd.DataFrame()
+      st.session_state["all_df"] = pd.DataFrame()
+      st.session_state["column_data"] = dict()
+      st.session_state["filtered_columns"] = list()
+
 # タブ
 spk.tab1.file_uploader("CSVファイルをアップロード", 
                   type=["csv"], 
